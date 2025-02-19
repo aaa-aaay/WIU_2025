d7d3fc67b90d7dc91a26874700395805dc68ac85
bananaman22s <im3dwinlee@gmail.com>
2025-02-19 14:49:57 +0800

    Day 2
    Boss AI 50%

---
+++ Assets/EdwinThings/Scripts/AlertBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AlertBehaviour : StateMachineBehaviour
+{
+    [SerializeField] private BossAI boss;
+    public States setState;
+
+    private void Awake()
+    {
+        GameObject bossObject = GameObject.Find("Rumba Dancing");
+        boss = bossObject.GetComponent<BossAI>();
+        if (boss == null)
+        {
+            Debug.LogWarning("BossAI instance not found!");
+        }
+    }
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("DONE");
+        boss.resetAnimationBools();
+        boss.setBossState(setState);
+    }
+}
---
+++ Assets/EdwinThings/Scripts/BossAI.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum States
+{ 
+    CHASE,
+    ATTACKING,
+    DEFEND,
+    VUNERABLE,
+    ATTACKED,
+    STUNNED,
+    DEATH
+}
+
+public class BossAI : MonoBehaviour
+{
+    [SerializeField] Transform playerPos;
+    private States currentState;
+    [SerializeField] Animator animator;
+    [SerializeField] SphereCollider attackCollider;
+    private bool shouldAttack;
+    private int whichAttack;
+
+    // Adjustable rotation speed
+    [SerializeField] float rotationSpeed = 5f;
+
+    void Start()
+    {
+        resetAnimationBools();
+        whichAttack = 0;
+        currentState = States.CHASE;
+        if (animator == null)
+        {
+            Debug.LogWarning("There aint no animator sir");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateMachine();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Attack");
+            currentState = States.ATTACKING;
+        }
+    }
+
+    public void resetAnimationBools()
+    {
+        Debug.Log("RESET");
+        animator.SetBool("IsChasing", false);
+        animator.SetBool("IsAttacking", false);
+    }
+
+    public void setBossState(States statename)
+    {
+        resetAnimationBools();
+        currentState = statename;
+    }
+
+    private void StateMachine()
+    {
+        switch (currentState)
+        {
+            case States.CHASE:
+                Vector3 direction = playerPos.position - transform.position;
+                direction.y = 0; 
+
+                if (direction.sqrMagnitude > 0.001f)
+                {
+                    Quaternion targetRotation = Quaternion.LookRotation(direction);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                }
+                if (!animator.GetBool("IsChasing"))
+                {
+                    animator.SetBool("IsChasing", true);
+                    Debug.Log("State: CHASE");
+                }
+                break;
+
+            case States.ATTACKING:
+                if (!animator.GetBool("IsAttacking"))
+                {
+                    Debug.Log("State: Attack");
+                    animator.SetBool("IsAttacking", true);
+                    whichAttack = UnityEngine.Random.Range(-1, 4);
+                    Debug.Log(whichAttack);
+                    animator.SetInteger("WhichAttack", whichAttack);
+                }
+
+
+                break;
+
+            case States.DEFEND:
+                // TODO: Add defend behavior
+                Debug.Log("State: DEFEND");
+                break;
+
+            case States.VUNERABLE:
+                // TODO: Add vulnerable behavior
+                Debug.Log("State: VUNERABLE");
+                break;
+
+            case States.ATTACKED:
+                // TODO: Add attacked behavior
+                Debug.Log("State: ATTACKED");
+                break;
+
+            case States.STUNNED:
+                // TODO: Add stunned behavior
+                Debug.Log("State: STUNNED");
+                break;
+
+            case States.DEATH:
+                // TODO: Add death behavior
+                Debug.Log("State: DEATH");
+                break;
+
+            default:
+                Debug.LogWarning("Unknown state encountered! / No state set");
+                break;
+        }
+    }
+}
